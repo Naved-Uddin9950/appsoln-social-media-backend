e9c2ecdb3e46964994cb8e4fca38a499fdbeb88d
naved.uddin <naved.uddin@appsoln.com>
2024-11-16 22:07:02 +0530

    minor change

--- index.js
+++ index.js
@@ -52,6 +52,10 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Server is running...')
+});
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
